8dbb8a7a0e09645c249603430f3cf51589acc39f
Riguz Lee <solee.linux@gmail.com>
2021-05-17 22:57:54 +0800

    migrate to dart null-safety

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,3 +2,7 @@
 
 * Support get opencv version for ios/android
 * Native c++ is used for Android integration 
+
+## 0.1.0
+
+* migrate to dart 2.12.0 (null safety)
--- lib/opencv_helper.dart
+++ lib/opencv_helper.dart
@@ -10,11 +10,11 @@ const MethodChannel _channel = const MethodChannel('com.riguz.opencv_helper');
 
 class OpenCVHelper {
   static Future<String> version() async {
-    return _channel.invokeMethod('version', {});
+    return await _channel.invokeMethod('version', {});
   }
 
   static Future<Uint8List> resize(String sourceImagePath, int width, int height) async {
-    return _channel.invokeMethod('resize', {
+    return await _channel.invokeMethod('resize', {
       "source": sourceImagePath,
       "width": width,
       "height": height,
